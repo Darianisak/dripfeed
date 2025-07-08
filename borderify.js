8bfdf2b5d65201bc48e2758518ecd1f8d769b40f
Darian Culver <culver.darian@gmail.com>
2025-07-08 22:08:49 +1200

    Save

--- borderify.js
+++ borderify.js
@@ -1,1 +1,14 @@
-document.body.style.border = "5px solid red";
+
+var url =  document.URL
+
+if (url.includes("reddit")) {
+    document.body.style.border = "5px solid red";
+}   else if (url.includes("instagram")) {
+    document.body.style.border = "5px solid yellow";
+}   else if (url.includes("linkedin")) {
+    document.body.style.border = "5px solid blue";
+}   else if (url.includes("facebook")) {
+    document.body.style.border = "5px solid purple";
+}   else {
+    document.body.style.border = "5px solid green";
+}
