d251ffd9aa605240a1ec8b545e8d01059536d7db
Darian Culver <culver.darian@gmail.com>
2025-07-09 09:06:11 +1200

    Save

--- borderify.js
+++ borderify.js
@@ -1,8 +1,39 @@
 
+// Wait for the DOM to be loaded before we try and remove nodes.
+//
+// FIXME : Need a timeout escape here incase page loads take too long.
+//      This is shit and will never resolve
+//
+// while (document.readyState !== "interactive" || document.readyState !== "complete") {
+    // async() => {await new Promise(r => setTimeout(r, 800))};
+    // console.log("Waiting for DOM to be interactive...");
+// 
+    // if (document.readyState === "complete") {
+        // break;
+    // }
+// };
+// 
+
 var url =  document.URL
 
+//  FIXME : Probably better to do this as a switch func
+//
 if (url.includes("reddit")) {
-    document.body.style.border = "5px solid red";
+    console.log("Applying tweaks to Reddit");
+
+    // Remove the infini-scroll area
+    //
+    // This ALSO removes post body, so we may need some further rules based on
+    // URL.
+    //
+    document.querySelector('.subgrid-container').remove();
+
+    // Remove the sidebar
+    //
+    document.querySelector('.left-sidebar').remove();
+
+    console.log("Tweaks applied.");
+
 }   else if (url.includes("instagram")) {
     document.body.style.border = "5px solid yellow";
 }   else if (url.includes("linkedin")) {
