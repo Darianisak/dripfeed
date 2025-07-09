7438effc5fed0acd0adae33c28820f57469a4c1a
Darian Culver <culver.darian@gmail.com>
2025-07-10 09:08:35 +1200

    save

--- borderify.js
+++ borderify.js
@@ -1,45 +1,34 @@
-
-// Wait for the DOM to be loaded before we try and remove nodes.
-//
-// FIXME : Need a timeout escape here incase page loads take too long.
-//      This is shit and will never resolve
-//
-// while (document.readyState !== "interactive" || document.readyState !== "complete") {
-    // async() => {await new Promise(r => setTimeout(r, 800))};
-    // console.log("Waiting for DOM to be interactive...");
-// 
-    // if (document.readyState === "complete") {
-        // break;
-    // }
-// };
-// 
-
 var url =  document.URL
+
+
+// This approach is a bit more robust in that it will always 'work' on page load
+// Do need to make the extension responsive to pageEvents, though, as navigating
+// within the site does not cause the extension to be fired again.
+//
+function removeNode(selectorTarget, dom)    {
+    var node = dom.querySelector(selectorTarget);
+
+    if (node === null) {
+        console.log(`${selectorTarget} could not be removed!`)
+        return;
+    }
+
+    node.remove();
+    console.log(`${selectorTarget} was removed.`)
+    return
+}
 
 //  FIXME : Probably better to do this as a switch func
 //
 if (url.includes("reddit")) {
     console.log("Applying tweaks to Reddit");
-
-    // Remove the infini-scroll area
+    removeNode('.subgrid-container', document);
+    removeNode('.left-sidebar', document);
+    // FIXME - it's not attached by default, so need a listener.
     //
-    // This ALSO removes post body, so we may need some further rules based on
-    // URL.
-    //
-    document.querySelector('.subgrid-container').remove();
-
-    // Remove the sidebar
-    //
-    document.querySelector('.left-sidebar').remove();
-
+    removeNode('.search-dropdown-results-container', document);
     console.log("Tweaks applied.");
 
-}   else if (url.includes("instagram")) {
-    document.body.style.border = "5px solid yellow";
-}   else if (url.includes("linkedin")) {
-    document.body.style.border = "5px solid blue";
-}   else if (url.includes("facebook")) {
-    document.body.style.border = "5px solid purple";
 }   else {
     document.body.style.border = "5px solid green";
 }
