1b7bcfc2f1d34774c42027f94bb52f63db141390
Darian Culver <culver.darian@gmail.com>
2025-12-17 07:01:41 +1300

    save

--- __tests__/instagram.test.js
+++ __tests__/instagram.test.js
@@ -1,8 +1,12 @@
+const find_common_ancestor = require("../ext/instagram");
+
 describe(".find_common_ancestor", () => {
   describe("function arguments", () => {
     test("ensures left_node is a valid element", () => {});
     test("ensures function requires two arguments", () => {});
     test("ensures mix-type arguments return null", () => {});
   });
-  test("ensures no match returns null", () => {});
+  test("ensures no match returns null", () => {
+    find_common_ancestor(1);
+  });
 });
