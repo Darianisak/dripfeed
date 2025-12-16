001516d140503d7d3d9270c50ec7cdb0506412b8
Darian Culver <culver.darian@gmail.com>
2025-12-16 22:35:47 +1300

    sve

--- __tests__/instagram-test.js
+++ __tests__/instagram-test.js
@@ -1,11 +1,8 @@
-
-describe('.find_common_ancestor', () => {
-
-    describe('function arguments', () => {
-        test('ensures left_node is a valid element', () => {});
-        test('ensures function requires two arguments', () => {});
-        test('ensures mix-type arguments return null', () => {});
-
-    });
-    test('ensures no match returns null', () => {});
+describe(".find_common_ancestor", () => {
+  describe("function arguments", () => {
+    test("ensures left_node is a valid element", () => {});
+    test("ensures function requires two arguments", () => {});
+    test("ensures mix-type arguments return null", () => {});
+  });
+  test("ensures no match returns null", () => {});
 });
