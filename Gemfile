58ba6851dab7625834dc66063fcdb68f3c5969e8
Darian Culver <culver.darian@gmail.com>
2026-02-14 13:27:24 +1300

    [WIP] Temporary history

--- Gemfile
+++ Gemfile
@@ -6,7 +6,10 @@ source "https://rubygems.org"
 gem "rspec", "~> 3.13"
 gem "capybara", "~> 3.40"
 gem "debug", "~> 1.11"
-gem "vcr", "~> 6.4"
 gem "stringio", "~> 3.2.0" 
 
 gem "selenium-webdriver", "~> 4.40"
+
+gem "vcr", "~> 6.4"
+
+gem "webmock", "~> 3.26"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,12 @@ RSpec.configure do |config|
   config.include Capybara::DSL
   Capybara.default_driver = :selenium
 end
+
+
+require 'webmock'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
