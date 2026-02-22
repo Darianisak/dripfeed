860ca2d9c77c4dbab4fc91c880f68d205f03f4e2
Darian Culver <culver.darian@gmail.com>
2026-02-22 23:56:12 +1300

    factory: dom tree scaffold
    Provides a factory function for generating a tree-like DOM structure, which is
    more appropriate for verifying hierarchical deletions.
    
    Think of a Pascals Triangle or Depth-first model - that's the general vibe of
    this.

--- __tests__/factory.html.js
+++ __tests__/factory.html.js
@@ -1,6 +1,6 @@
 "use strict";
 
-export { build_n_layer_dom };
+export { build_n_layer_dom, build_tree_dom };
 
 function build_n_layer_dom(depth = 5) {
   const rootNode = document.createElement("div");
@@ -10,7 +10,6 @@ function build_n_layer_dom(depth = 5) {
   for (let layer = 0; layer < depth; layer++) {
     let childNode = document.createElement("div");
     childNode.id = `child-${layer}`;
-    // FIXME - bug here which means the DOM tree is split in half.
     currentNode.append(childNode);
     currentNode = childNode;
   }
@@ -18,4 +17,44 @@ function build_n_layer_dom(depth = 5) {
   document.body.append(rootNode);
 }
 
-// TODO build
+function build_tree_dom(depth = 5) {
+  const buildChildNode = (layer, childIndex) => {
+    if (!(typeof layer === "number" && typeof childIndex === "number")) {
+      throw new TypeError(
+        `invalid index or layer references were passed to #buildChildNode`,
+      );
+    }
+
+    let node = document.createElement("div");
+    node.id = `layer-${layer}_index-${childIndex}`;
+    return node;
+  };
+
+  const topLevelElement = document.createElement("div");
+  topLevelElement.id = `layer-0_index-0`;
+  document.body.append(topLevelElement);
+
+  var currentNode;
+
+  for (let layer = 0; layer < depth; layer++) {
+    // This kind of relates to the nth+1 iteration of the for loop
+    // below, in that we use it to define layerIndex ids ahead of time.
+    var currentLayerChildIndex = 0;
+
+    //  Given each node on a layer, we want to iterate through them
+    // and append 2 further DOM elements.
+    for (let layerIndex = 0; layerIndex < layer + 1; layerIndex++) {
+      currentNode = document.getElementById(
+        `layer-${layer}_index-${layerIndex}`,
+      );
+
+      // Left child node of currentNode
+      currentNode.append(buildChildNode(layer + 1, currentLayerChildIndex));
+      currentLayerChildIndex++;
+
+      // Right child node of currentNode
+      currentNode.append(buildChildNode(layer, currentLayerChildIndex));
+      currentLayerChildIndex++;
+    }
+  }
+}
