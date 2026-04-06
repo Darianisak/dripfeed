5ec53a8001fdf3534ab554be73a680a9dfea87c1
Darian Culver <culver.darian@gmail.com>
2026-04-06 21:14:39 +1200

    chore: rename `ext` to `src`
    Moves our extension source code dir to something that is more in-line with
    webpack and broader industry best practice.

--- sinatra.rb
+++ sinatra.rb
@@ -4,7 +4,7 @@ require "sinatra"
 require "sinatra/namespace"
 
 set :views, File.expand_path("views", __dir__)
-set :public_folder, File.expand_path("ext", __dir__)
+set :public_folder, File.expand_path("src", __dir__)
 
 namespace "/reddit" do
   get "/homepage" do
