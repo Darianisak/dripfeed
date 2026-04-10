dbbc3af457d4690d2003c9f473e94170e92c5fad
Darian Culver <culver.darian@gmail.com>
2026-04-11 02:11:54 +1200

    feat: apply extension to www.reddit.com
    Adds domain support to index.js, which means the extension will now correctly
    route to and modify DOM content on Reddit subpages.
    
    Quirks with script triggering post redirect and initialisation remain, but I
    think we'll need to implement additinonal sites to narrow in on that one.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import { getDomain } from "./helpers/pathHelper.js";
+import * as reddit from "./reddit/index.js";
 
 function extensionRouting(document) {
   const currentDomain = getDomain(document);
 
   switch (currentDomain) {
+    case "www.reddit.com":
+      reddit.routing(document);
+      break;
+
     default:
       console.warn(`'${currentDomain}' is not yet supported.`);
       return 127;
   }
 }
 
+// Handles initial page load
 document.addEventListener("DOMContentLoaded", () => extensionRouting(document));
 
 export { extensionRouting };
